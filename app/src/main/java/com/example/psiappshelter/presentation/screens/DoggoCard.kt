9ec28dea21bf1179b61fc00ae9751aa858d55014
nor0509 <167141010+nor0509@users.noreply.github.com>
2025-01-05 03:48:48 +0100

    feat(app): add profile, settings, and dog details screens
    - Added profile screen
    - Added settings screen
    - Added detailed dog information screen
    - Moved the list to ViewModel for better state management
    - Improved aesthetics by adding gradients to dog avatars
    - Added search functionality with validation to check if a dog is already in the ViewModel
    - Added a dividing line between dogs in the list
    - Added 3 example dogs displayed at the start of the program
    - Improved the functionality of NavHost
    - Final adjustments made to meet specified requirements

--- app/src/main/java/com/example/psiappshelter/presentation/screens/DoggoCard.kt
+++ app/src/main/java/com/example/psiappshelter/presentation/screens/DoggoCard.kt
@@ -27,12 +27,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.psiappshelter.Doggo
 import com.example.psiappshelter.R
+import com.example.psiappshelter.ui.theme.GradientPurple
 
 @Composable
 fun DoggoCard(
@@ -53,8 +55,10 @@ fun DoggoCard(
             modifier = Modifier
                 .size(48.dp)
                 .background(
-                    color = MaterialTheme.colorScheme.primary,
-                    shape = RoundedCornerShape(12.dp)
+                    brush = Brush.linearGradient(
+                        colorStops = GradientPurple
+                    ),
+                    shape = RoundedCornerShape(6.dp)
                 ),
             contentAlignment = Alignment.Center
         ){
@@ -170,4 +174,23 @@ fun AddDoggoDialog(
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DoggoCardPreview() {
+    MaterialTheme {
+        DoggoCard(
+            doggo = Doggo(
+                name = "Pan Punpernikiel",
+                breed = "Jack Russel",
+                age = 3,
+                isFavorite = false
+            ),
+            onFavoriteClick = {},
+            onDeleteClick = {},
+            onCardClick = {},
+            modifier = Modifier
+        )
+    }
 }
--- app/src/main/java/com/example/psiappshelter/ui/theme/Color.kt
+++ app/src/main/java/com/example/psiappshelter/ui/theme/Color.kt
@@ -5,7 +5,12 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
+val PurpleDark = Color(0xff68588f)
+val PinkLight = Color(0xffeeb6e8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GradientPurple = arrayOf(
+    0.3f to PurpleDark,
+    0.7f to PinkLight)
